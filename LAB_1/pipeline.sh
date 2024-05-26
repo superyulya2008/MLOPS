0b7e9d3e939ee7ebac1dd26d2560e7c05d260f6a
superyulya2008 <148714565+superyulya2008@users.noreply.github.com>
2024-05-26 17:23:10 +0300

    Update pipeline.sh

--- LAB_1/pipeline.sh
+++ LAB_1/pipeline.sh
@@ -1,1 +1,18 @@
+bash
+#!/bin/bash
+
+#echo "Создание данных для обучения"
+python data_creation.py train
+
+#echo "Создание данных для тестирования"
+python data_creation.py test
+
+#echo "Предобработка данных"
+python model_preprocessing.py
+
+#echo "Обучение модели"
+python model_preparation.py
+
+#echo "Тестирование модели"
+python model_testing.py
 
